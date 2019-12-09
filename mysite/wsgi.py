02d7bf459c845dbd0dcc96172058b6e826a7069a
Sgudkov <sgudkovv@gmail.com>
2019-12-09 20:38:03 +0500

    Check ignore 2

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -19,4 +19,4 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-#another commit
+#another commit2
